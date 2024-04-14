4302c13b93151da622d786ae0f96e6a07974cf7b
jamoowen <jamow.owen@gmail.com>
2024-04-14 21:36:09 +0100

    crypto jobs list working along with local csv write

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,36 +1,40 @@
 import fs from 'fs'
 
 /**
- * Writes data to a CSV file locally.
+ * Writes data to a TSV file locally.
  * 
- * @param fileName - The name of the CSV file.
- * @param directory - The directory path where the CSV file will be saved.
+ * @param fileName - The name of the TSV file.
+ * @param directory - The directory path where the TSV file will be saved.
  * @param columnNames - An array containing the names of the columns in the CSV file.
  * @param data - A 2-dimensional array containing the data to be written to the CSV file.
  * @returns Void.
- * @remarks This function creates a CSV string from the provided data and writes it to a file
+ * @remarks This function creates a TSV string from the provided data and writes it to a file
  * specified by the fileName and directory parameters. Each row of data is joined using the pipe
  * ('|') separator, and each column name is joined to form the header row of the CSV file.
  * 
  * @example
  * ```typescript
- * writeCsvLocally('data.csv', '/path/to/directory/', ['Name', 'Age', 'Country'], [['John', '30', 'USA'], ['Alice', '25', 'Canada']]);
+ * writeTsvLocally('data.csv', '/path/to/directory/', ['Name', 'Age', 'Country'], [['John', '30', 'USA'], ['Alice', '25', 'Canada']]);
  * ```
  */
-export function writeCsvLocally(fileName: string, directory: string,  columnNames: string[] , data: string[][]) {
-    let csvString = columnNames.join('|') + '\n'
+export function writeTsvLocal(fileName: string, directory: string, columnNames: string[], data: string[][]) {
+  let csvString = columnNames.join('\t') + '\n'
 
-    const formattedData = data.map((row) => row.join('|'));
-    const formattedRows = formattedData.join('\n')
+  const formattedData = data.map((row) => row.join('\t').replace('\n', ''));
+  const formattedRows = formattedData.join('\n')
+  // console.log(`initial: ${data}`)
+  // console.log(`formatted data: ${formattedData}`)
+  // console.log(`formatted: ${formattedRows}`)
 
-    csvString = csvString + formattedRows
+  csvString = csvString + formattedRows
 
-    try {
-        fs.writeFileSync(directory + fileName, csvString);
-        console.log(`file successfully written to: ${directory+fileName}`)
-      } catch (err) {
-        console.error(`Error writing file to: ${directory + fileName}, ${err}`);
-      }
-      
+  try {
+    fs.writeFileSync(directory + fileName, csvString);
+    console.log(`file successfully written to: ${directory + fileName}`)
+  } catch (err) {
+    console.error(`Error writing file to: ${directory + fileName}, ${err}`);
+  }
+
+
 
 }
--- src/sources/CryptoJobsList.ts
+++ src/sources/CryptoJobsList.ts
@@ -1,199 +1,224 @@
 
-    // JOB DATA
-    import axios from 'axios';
-    import * as cheerio from 'cheerio';
-    import { writeCsvLocally } from '../lib/helpers';
-    // import { CryptoJobsListData } from './types';
-    
-    // https://cryptojobslist.com/api/jobs/backend-engineer-level-2-sde-1-coinshift-bangalore
-    
-    const urlsToScrape = [
-        'https://cryptojobslist.com/?sort=recent&page=1',
-        'https://cryptojobslist.com/?sort=recent&page=2',
-        'https://cryptojobslist.com/?sort=recent&page=3',
-        'https://cryptojobslist.com/?sort=recent&page=4',
-        'https://cryptojobslist.com/?sort=recent&page=5',
-        'https://cryptojobslist.com/?sort=recent&page=6',
-        'https://cryptojobslist.com/?sort=recent&page=7',
-        'https://cryptojobslist.com/?sort=recent&page=8',
-        
+// JOB DATA
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { writeTsvLocal } from '../lib/helpers';
+// import { CryptoJobsListData } from './types';
+
+// https://cryptojobslist.com/api/jobs/backend-engineer-level-2-sde-1-coinshift-bangalore
+
+const urlsToScrape = [
+    'https://cryptojobslist.com/?sort=recent&page=1',
+    'https://cryptojobslist.com/?sort=recent&page=2',
+    'https://cryptojobslist.com/?sort=recent&page=3',
+    'https://cryptojobslist.com/?sort=recent&page=4',
+    'https://cryptojobslist.com/?sort=recent&page=5',
+    'https://cryptojobslist.com/?sort=recent&page=6',
+    'https://cryptojobslist.com/?sort=recent&page=7',
+    'https://cryptojobslist.com/?sort=recent&page=8',
+
+]
+
+
+
+interface LocationSchema {
+    '@type': string;
+    address: {
+        [key: string]: any;
+    };
+}
+
+
+interface Job {
+    id: string;
+    jobTitle: string;
+    jobDescription: string;
+    jobLocation: string;
+    locationSchema: LocationSchema;
+    salaryRange: string | null;
+    salary: number | null;
+    category: string;
+    tags: string[];
+    company: any;
+    companyId: string;
+    companyAbout: string;
+    companyName: string;
+    companyTagline: string | null;
+    companyLogo: string;
+    companyTwitter: string | null;
+    companyDiscord: string | null;
+    companySlug: string;
+    companyVerified: boolean;
+    logoDarkMode: string | null;
+    companyUrl: string;
+    coinSymbol: string | null;
+    coinGeckoCoinId: string | null;
+    remote: boolean;
+    timezone: string | null;
+    employmentType: string[];
+    paysInCrypto: string | null;
+    paysInFiat: string | null;
+    paidRelocation: boolean;
+    visaSponsor: boolean;
+    skills: string[] | null;
+    bossPicture: string | null;
+    bossName: string | null;
+    bossFirstName: string | null;
+    bossLastName: string | null;
+    bossAbout: string | null;
+    bitlyLink: string;
+    randomSuffix: string;
+    telegramDiscussionLink: string;
+    telegramDiscussionRepliesQty: number | null;
+    canonicalURL: string | null;
+    seoSlug: string;
+    resumeRequired: boolean | null;
+    applicationLink: string;
+    ogImageUrl: string;
+    question1: string | null;
+    question2: string | null;
+    question3: string | null;
+    totalViewsCount: number;
+    applicationCountV2: number | null;
+    applicationLinkClicks: number;
+    directApplicationsQty: number;
+    isFeatured: boolean;
+    featuredAt: string | null;
+    publishedAt: string;
+    createdAt: string;
+    published: boolean;
+    lastApplicationAt: string;
+    filled: boolean;
+    filledAt: string | null;
+    bid: number;
+    locationEnriched: string | null;
+    locationEnhancedStr: string | null;
+    locationEnhancedObj: string | null;
+    videoApplicationsEnabled: boolean | null;
+    videoApplicationsRequired: boolean | null;
+    removed: boolean;
+    notAJobAd: boolean | null;
+    totalJobApplications: number;
+    applicationQuantityTitle: string;
+    isActive: boolean;
+}
+
+
+
+interface CryptoJobsListData {
+    job: Job;
+    relatedJobs: any;
+    locations: any[];
+    jobPostingJSONLD: any;
+    jobStats: boolean;
+}
+
+async function fetchJobsList() {
+    const links = [];
+
+    for (let url of urlsToScrape) {
+        try {
+            const { data } = await axios.get(url);
+            const $ = cheerio.load(data);
+            const jobs: any = $("tr > td > div > a");
+
+            jobs.each((index, value) => {
+                const link = $(value).attr('href');
+                links.push(link);
+            });
+
+
+            console.log(`Links retrieved for ${url}`);
+        } catch (error) {
+            console.error(`Error pulling job links from ${url}: ${error}`);
+        }
+
+    }
+    return links
+}
+
+async function fetchJobData(links: string[]) {
+    const dateFound = new Date().toISOString();
+
+    const jobData = []
+
+    const apiPrefix = 'https://cryptojobslist.com/api/jobs/'
+    for (let link of links) {
+        const url = apiPrefix + link.split('/').slice(2).join('/')
+        console.log(`fetching data from: ${url}`)
+
+        // datePosted, title, company, description, additionalInfo, salary, salaryRange, contractType, perks, location, country, applicationLink, emailApplication, tags, dateFound, numLinkClicks
+        try {
+            const data: Job = (await axios.get(url)).data.job;
+
+            const trimmedData = [
+                data.publishedAt,
+                data.jobTitle,
+                data.companyName,
+                `${data.jobDescription.trim()}`,
+                '',
+                data.salary,
+                data.salaryRange,
+                data.employmentType,
+                '',
+                data.jobLocation,
+                '',
+                data.applicationLink,
+                '',
+                data.tags,
+                data.applicationLinkClicks,
+                dateFound
+            ]
+
+            jobData.push(trimmedData)
+            // console.log(`description: ${data.jobDescription}`)
+
+        } catch (error) {
+            console.error(`Error pulling job data for ${url}: ${error}`);
+
+        }
+    }
+    return jobData
+
+
+}
+
+
+
+
+
+export async function cryptoJobsList() {
+
+    const columnNames = [
+        "date_posted",
+        "job_title",
+        "company",
+        "job_description",
+        "additional_info",
+        "salary",
+        "salary_range",
+        "contractType",
+        "perks",
+        "location",
+        "country",
+        "application_link",
+        "emailApplication",
+        "tags",
+        "num_applications",
+        "date_found"
     ]
-    
-    
-    
-    interface LocationSchema {
-        '@type': string;
-        address: {
-            [key: string]: any;
-        };
-    }
-    
-    
-    interface Job {
-        id: string;
-        jobTitle: string;
-        jobDescription: string;
-        jobLocation: string;
-        locationSchema: LocationSchema;
-        salaryRange: string | null;
-        salary: number | null;
-        category: string;
-        tags: string[];
-        company: any;
-        companyId: string;
-        companyAbout: string;
-        companyName: string;
-        companyTagline: string | null;
-        companyLogo: string;
-        companyTwitter: string | null;
-        companyDiscord: string | null;
-        companySlug: string;
-        companyVerified: boolean;
-        logoDarkMode: string | null;
-        companyUrl: string;
-        coinSymbol: string | null;
-        coinGeckoCoinId: string | null;
-        remote: boolean;
-        timezone: string | null;
-        employmentType: string[];
-        paysInCrypto: string | null;
-        paysInFiat: string | null;
-        paidRelocation: boolean;
-        visaSponsor: boolean;
-        skills: string[] | null;
-        bossPicture: string | null;
-        bossName: string | null;
-        bossFirstName: string | null;
-        bossLastName: string | null;
-        bossAbout: string | null;
-        bitlyLink: string;
-        randomSuffix: string;
-        telegramDiscussionLink: string;
-        telegramDiscussionRepliesQty: number | null;
-        canonicalURL: string | null;
-        seoSlug: string;
-        resumeRequired: boolean | null;
-        applicationLink: string;
-        ogImageUrl: string;
-        question1: string | null;
-        question2: string | null;
-        question3: string | null;
-        totalViewsCount: number;
-        applicationCountV2: number | null;
-        applicationLinkClicks: number;
-        directApplicationsQty: number;
-        isFeatured: boolean;
-        featuredAt: string | null;
-        publishedAt: string;
-        createdAt: string;
-        published: boolean;
-        lastApplicationAt: string;
-        filled: boolean;
-        filledAt: string | null;
-        bid: number;
-        locationEnriched: string | null;
-        locationEnhancedStr: string | null;
-        locationEnhancedObj: string | null;
-        videoApplicationsEnabled: boolean | null;
-        videoApplicationsRequired: boolean | null;
-        removed: boolean;
-        notAJobAd: boolean | null;
-        totalJobApplications: number;
-        applicationQuantityTitle: string;
-        isActive: boolean;
-    }
-    
-    
-    
-    interface CryptoJobsListData {
-        job: Job;
-        relatedJobs: any;
-        locations: any[];
-        jobPostingJSONLD: any;
-        jobStats: boolean;
-    }
-    
-    async function fetchJobsList() {
-        const links = [];
-    
-        for (let url of urlsToScrape) {
-            try {
-                const { data } = await axios.get(url);
-                const $ = cheerio.load(data);
-                const jobs: any = $("tr > td > div > a");
-    
-                jobs.each((index, value) => {
-                    const link = $(value).attr('href');
-                    links.push(link);
-                });
-    
-    
-                console.log(`Links retrieved for ${url}`);
-            } catch (error) {
-                console.error(`Error pulling job links from ${url}: ${error}`);
-            }
-    
-        }
-        return links
-    }
-    
-    async function fetchJobData(links: string[]) {
-        const dateFound = new Date().toISOString();
-    
-        const jobData = []
-    
-        const apiPrefix = 'https://cryptojobslist.com/api/jobs/'
-        for (let link of links) {
-            const url = apiPrefix + link.split('/').slice(2).join('/')
-            console.log(`fetching data from: ${url}`)
-            
-            // datePosted, title, company, description, additionalInfo, salary, salaryRange, contractType, perks, location, country, applicationLink, emailApplication, tags, dateFound, numLinkClicks
-            try {
-                const data: Job = (await axios.get(url)).data.job;
-    
-                const trimmedData = [
-                    data.publishedAt,
-                    data.jobTitle,
-                    data.companyName,
-                    data.jobDescription,
-                    '',
-                    data.salary,
-                    data.salaryRange,
-                    data.employmentType,
-                    '',
-                    data.jobLocation,
-                    '',
-                    data.applicationLink,
-                    '',
-                    data.tags,
-                    data.applicationLinkClicks,
-                    dateFound
-                ]
-                
-                jobData.push(trimmedData)
-    
-            } catch (error) {
-                console.error(`Error pulling job data for ${url}: ${error}`);
-    
-            }
-        }
-        return jobData
-    
-    
-    }
-    
-    
-    
-    
-    export async function cryptoJobsList() {
-        const today = new Date().toISOString().
-        // const links = await fetchJobsList()
-        // console.log(links)
-        const jobData = await fetchJobData(['/developer/blockchain-developer-algorithm-global-amber-global'])
-        console.log(`writing job data to csv`)
-        await writeCsvLocally(`crypto-jobs-list-${}`)
-        // console.log(`jobdata length: ${jobData}`)
-    }
-    
-    
-    
+
+    const today = new Date().toISOString()
+    console.log(`today: ${today}`)
+
+    // const links = await fetchJobsList()
+    // console.log(links)
+    const jobData = await fetchJobData(['/developer/blockchain-developer-algorithm-global-amber-global'])
+    console.log(`writing job data to csv`)
+    writeTsvLocal(`crypto-jobs-list-${today}.tsv`, '/Users/jowen/Desktop/smarterjobs/data-extraction/output/', columnNames, jobData)
+
+    // console.log(`jobdata length: ${jobData}`)
+}
+
+
+
